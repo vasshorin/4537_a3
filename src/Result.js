559c55ae233a23e72e46e3d2eb31439c6d0bfdc4
vasshorin <)tSz%gBWz1>
2023-03-30 20:30:23 -0700

    Add css to results, pokemon card

--- src/Result.js
+++ src/Result.js
@@ -5,6 +5,7 @@ import './Result.css'
 function Result({ selectedTypes, PAGE_SIZE, setCurrentPage, currentPage }) {
 
   const [pokemons, setPokemons] = useState([])
+  const [selectedPokemon, setSelectedPokemon] = useState(null)
 
   useEffect(() => {
     async function fetchData() {
@@ -23,11 +24,66 @@ function Result({ selectedTypes, PAGE_SIZE, setCurrentPage, currentPage }) {
   const paginate = (pageNumber) => setCurrentPage(pageNumber)
   const numPages = Math.ceil(filteredPokemons.length / PAGE_SIZE);
 
+  const handlePokemonClick = (pokemon) => {
+    setSelectedPokemon(pokemon)
+  }
+
+  const handleClosePokemon = () => {
+    setSelectedPokemon(null)
+  }
+
   return (
+    <div>
+      {selectedPokemon && (
+        <div className="pokemon-modal">
+          <div className="pokemon-modal-content">
+            <button className="pokemon-modal-close" onClick={handleClosePokemon}>
+              X
+            </button>
+            <div className="pokemon-modal-img">
+              <img
+                src={`https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/${String(selectedPokemon.id).padStart(3, '0')}.png`}
+                alt={selectedPokemon.name.english}
+              />
+            </div>
+            <div className="pokemon-modal-stats">
+              <h2>{selectedPokemon.name.english}</h2>
+              <div className="pokemon-modal-stat">
+                <p>Type:</p>
+                <p>{selectedPokemon.type.join(', ')}</p>
+              </div>
+              <div className="pokemon-modal-stat">
+                <p>Attack:</p>
+                <p>{selectedPokemon.base.Attack}</p>
+              </div>
+              <div className="pokemon-modal-stat">
+                <p>Defense:</p>
+                <p>{selectedPokemon.base.Defense}</p>
+              </div>
+              <div className="pokemon-modal-stat">
+                <p>HP:</p>
+                <p>{selectedPokemon.base.HP}</p>
+              </div>
+              <div className="pokemon-modal-stat">
+                <p>Sp. Attack:</p>
+                <p>{selectedPokemon.base['Sp. Attack']}</p>
+              </div>
+              <div className="pokemon-modal-stat">
+                <p>Sp. Defense:</p>
+                <p>{selectedPokemon.base['Sp. Defense']}</p>
+              </div>
+              <div className="pokemon-modal-stat">
+                <p>Speed:</p>
+                <p>{selectedPokemon.base.Speed}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     <div className="pokemon-grid">
       {
         currentPokemons.map(pokemon => (
-          <div key={pokemon.id} className="pokemon-card">
+          <div key={pokemon.id} className="pokemon-card" onClick={() => handlePokemonClick(pokemon)}>
             <img
               src={`https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/${String(pokemon.id).padStart(
                 3,
@@ -40,6 +96,7 @@ function Result({ selectedTypes, PAGE_SIZE, setCurrentPage, currentPage }) {
           </div>
         ))
       }
+    </div>
       <Pagination pokemonPerPage={PAGE_SIZE} totalPokemon={filteredPokemons.length} currentPage={currentPage} numPages={numPages} paginate={paginate} />
     </div>
   )
