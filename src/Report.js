2816095ed1fb8735e785e66342da0a343b9e56fb
vasshorin <)tSz%gBWz1>
2023-04-02 09:52:17 -0700

    Add unique users and top users

--- src/Report.js
+++ src/Report.js
@@ -1,61 +1,123 @@
-import React from "react";
 import axios from 'axios'
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
 import jwt_decode from "jwt-decode";
 
 function Report({id, accessToken, setAccessToken, refreshToken}) {
-  
-const [reportTable, setReportTable] = useState(null);
-  
-// Add a request interceptor
-const axiosToBeIntercepted = axios.create()
-axiosToBeIntercepted.interceptors.request.use(async function (config) {
-  // Do something before request is sent
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  );
 
-  // try to refresh token
-  // check if token is expired
-  const decoded = jwt_decode(accessToken);
-  const currentTime = Date.now() / 1000;
-  if (decoded.exp < currentTime) {
-    console.log("token expired")
-    // send a request to refresh token
-    const res = await axios.get('http://localhost:5003/requestNewAccessToken',
-      {
-        headers: {
-          "auth-token-refresh": refreshToken
-        }
-      })
-    setAccessToken(res.headers["auth-token-access"])
-    config.headers["auth-token-access"] = res.headers["auth-token-access"]
-  }
+  const [chartData, setChartData] = useState(null);
 
-  return config;
-}, function (error) {
-  // Do something with request error
-  return Promise.reject(error);
-});
-  
-useEffect(() => {
-    async function fetchReport() {
-      const res = await axiosToBeIntercepted.get(
-        `http://localhost:6003/report?id=${id}`
-        , {
+  // Add a request interceptor
+  const axiosToBeIntercepted = axios.create()
+  axiosToBeIntercepted.interceptors.request.use(async function (config) {
+
+    const decoded = jwt_decode(accessToken);
+    const currentTime = Date.now() / 1000;
+    if (decoded.exp < currentTime) {
+      console.log("token expired")
+      // send a request to refresh token
+      const res = await axios.get('http://localhost:5003/requestNewAccessToken',
+        {
           headers: {
-            "auth-token-access": accessToken
+            "auth-token-refresh": refreshToken
           }
         })
-      setReportTable(res.data)
+      setAccessToken(res.headers["auth-token-access"])
+      config.headers["auth-token-access"] = res.headers["auth-token-access"]
     }
-    fetchReport()
-  }, [accessToken, axiosToBeIntercepted, id])
-  
- return (
-    <div>
-      Report {id} 
-      {
-        (reportTable) &&
-         reportTable
+
+    return config;
+  }, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  });
+
+  useEffect(() => {
+    async function fetchReport() {
+      let res;
+      if (id === 1) {
+        // Perform the MongoDB aggregation query for Report 1
+        res = await axiosToBeIntercepted.get("http://localhost:6003/logs/unique-api-users", {
+          headers: {
+            "auth-token-access": accessToken,
+          },
+        });
+        console.log(res.data)
+        // modify the data and labels arrays for the Bar chart
+        if (res.data) {
+          const labels = res.data.map(item => `${item._id.day}-${item._id.month}-${item._id.year}`);
+          const data = res.data.map(item => item.count);
+          setChartData({
+            labels: labels,
+            datasets: [
+              {
+                label: 'Number of Unique API Users',
+                data: data,
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132, 1)',
+                borderWidth: 1,
+              },
+            ],
+          });
+        }
+      } else if (id === 2) {
+        console.log("id is 2")
+        res = await axiosToBeIntercepted.get(`http://localhost:6003/log/top-api-users`, {
+          headers: {
+            "auth-token-access": accessToken,
+          },
+        });
+        console.log(res.data)
+        // modify the data and labels arrays for the Bar chart
+        if (res.data) {
+          const data = res.data.map(item => item.count);
+          const labels = res.data.map(item => item._id);
+          setChartData({
+            labels: labels,
+            datasets: [
+              {
+                label: 'Top API Users',
+                data: data,
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132, 1)',
+                borderWidth: 1,
+              },
+            ],
+          });
+        }
+      } else {
+        // Fetch the report data from the server for other reports
+        res = await axiosToBeIntercepted.get(`http://localhost:6003/report?id=${id}`, {
+          headers: {
+            "auth-token-access": accessToken,
+          },
+        });
       }
+    }
+    fetchReport();
+  }, [accessToken, axiosToBeIntercepted, id]);
+
+  return (
+    <div>
+      Report {id}
+      {chartData ? <Bar data={chartData} /> : <p>Loading chart data...</p>}
     </div>
   );
 }
